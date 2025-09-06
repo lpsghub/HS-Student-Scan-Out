362f4ba11ca7ce6dad4a82cd5c699b231f343933
lpsghub <sdelfino@lindenwold.k12.nj.us>
2025-09-05 23:27:10 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 window.APP_CONFIG = \{\
+  // Your live Apps Script Web App URL:\
+  API_BASE: 'https://script.google.com/macros/s/1twuB9YsxBwyTikmRgkhQqH5swr780LzolEvUXMGIQdk/exec',\
+  SOURCE:   'Front Desk',\
+  DEVICE_ID:'KIOSK-1',\
+  AUTO_CLEAR_MS: 3000\
+\};\
+}
